ff6b70f19c72c8894eb18214b9198abb353145e4
Sasa Nedic <svetasmak0@gmail.com>
2023-12-25 23:42:30 +0100

    Create some changes in code

--- Landing page/script.js
+++ Landing page/script.js
@@ -1,3 +1,20 @@
+const menuItems = [
+  {
+    name: "Magnem Tirste",
+    ingredients: "Lorem, deren, trataro, filede, nerada",
+    price: "$4.95",
+  },
+  {
+    name: "Aut Luia",
+    ingredients: "Lorem, deren, trataro, filede, nerada",
+    price: "$14.95",
+  },
+  {
+    name: "Est Eligendi",
+    ingredients: "Lorem, deren, trataro, filede, nerada",
+    price: "$8.95",
+  },
+];
 const burger = document.querySelector(".burger");
 const navElements = document.querySelector("ul");
 
