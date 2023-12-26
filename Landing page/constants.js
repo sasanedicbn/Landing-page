8d5a84c9ea81c4b8e42db8bb43de0f7846972ace
Sasa Nedic <svetasmak0@gmail.com>
2023-12-26 18:58:39 +0100

    Create helper file and add all images

---
+++ Landing page/constants.js
@@ -0,0 +1,38 @@
+export const menuItems = [
+  {
+    photo: "./img/breakfast-1.png",
+    name: "Magnem Tirste",
+    ingredients: "Lorem, deren, trataro, filede, nerada",
+    price: "$4.95",
+  },
+  {
+    photo: "./img/Eos liubustam fifth.png",
+    name: "Aut Luia",
+    ingredients: "Lorem, deren, trataro, filede, nerada",
+    price: "$14.95",
+  },
+  {
+    photo: "./img/Eos salad fourth.png",
+    name: "Est Eligendi",
+    ingredients: "Lorem, deren, trataro, filede, nerada",
+    price: "$8.95",
+  },
+  {
+    photo: "./img/est-eligendi-third.png",
+    name: "Eos salad fourth",
+    ingredients: "Lorem, deren, trataro, filede, nerada",
+    price: "$7.45",
+  },
+  {
+    photo: "./img/Laboroisam devida.png",
+    name: "Eos liubustam",
+    ingredients: "Lorem, deren, trataro, filede, nerada",
+    price: "$12.95",
+  },
+  {
+    photo: "./img/Aut-Luia-second.png",
+    name: "Laboroisam devida",
+    ingredients: "Lorem, deren, trataro, filede, nerada",
+    price: "$8.95",
+  },
+];
--- Landing page/script.js
+++ Landing page/script.js
@@ -1,35 +1,5 @@
-const menuItems = [
-  {
-    name: "Magnem Tirste",
-    ingredients: "Lorem, deren, trataro, filede, nerada",
-    price: "$4.95",
-  },
-  {
-    name: "Aut Luia",
-    ingredients: "Lorem, deren, trataro, filede, nerada",
-    price: "$14.95",
-  },
-  {
-    name: "Est Eligendi",
-    ingredients: "Lorem, deren, trataro, filede, nerada",
-    price: "$8.95",
-  },
-  {
-    name: "Eos salad fourth",
-    ingredients: "Lorem, deren, trataro, filede, nerada",
-    price: "$7.45",
-  },
-  {
-    name: "Eos liubustam",
-    ingredients: "Lorem, deren, trataro, filede, nerada",
-    price: "$12.95",
-  },
-  {
-    name: "Laboroisam devida",
-    ingredients: "Lorem, deren, trataro, filede, nerada",
-    price: "$8.95",
-  },
-];
+import { menuItems } from "./constants";
+
 const burger = document.querySelector(".burger");
 const navElements = document.querySelector("ul");
 const displayMenuItems = document.querySelector(".container-menu-items");
@@ -46,10 +16,11 @@ function generateMenu(menuItems) {
     .map(
       (item) =>
         `
-    <div>
-      <h3>${item.name}</h3>
-      <p>${item.ingredients}</p>
-      <p>${item.price}</p>
+    <div class="meals">
+     <img src="${item.photo}" id="img"alt="${item.photo}"/>
+      <h3 class="menuTitle">${item.name}</h3>
+      <p class="menuIngredients">${item.ingredients}</p>
+      <p class="menuPrice">${item.price}</p>
     </div>
   `
     )
